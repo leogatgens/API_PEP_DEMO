c7e49db38612f5035fec643ae03b6f4b65debfde
Leonardo Meléndez <leogatgens@gmail.com>
2019-05-22 13:42:42 -0600

    permitir CORS

--- API/Startup.cs
+++ API/Startup.cs
@@ -25,12 +25,19 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+           
+            app.UseCors(builder =>
+                builder.WithOrigins("http://localhost:3000")
+               .AllowAnyHeader()
+               .AllowAnyMethod()
+           );
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
