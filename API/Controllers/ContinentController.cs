345adf79c33337e4558ec8db43bbee3574e6b021
Leonardo Meléndez <leogatgens@gmail.com>
2019-06-07 17:43:46 -0600

    ajustes para nuevo api

---
+++ API/Controllers/ContinentController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/continent")]
+    [ApiController]
+    public class ContinentController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            HashSet<string> continentes = new HashSet<string>();
+            continentes.Add("Europa");
+            continentes.Add("America");
+            continentes.Add("Asia");
+            continentes.Add("Africa");
+            continentes.Add("Oceania");
+
+            
+            //System.Threading.Thread.Sleep(5000);
+            return Ok(continentes);
+
+
+        }
+
+        
+        
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- API/Controllers/ValuesController.cs
+++ API/Controllers/ValuesController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/paises")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
@@ -14,7 +14,145 @@ namespace API.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            List<Pais> paisesEuropa = ObtieneEuropePaises();
+            List<Pais> paisesAmerica = ObtienePaisesDeAmerica();
+
+            List<Pais> finalList = paisesEuropa.Concat(paisesAmerica).ToList();
+            //System.Threading.Thread.Sleep(5000);
+            return Ok(finalList);
+
+        }
+
+        private static List<Pais> ObtieneEuropePaises()
+        {
+            return new List<Pais>
+            {
+                new Pais
+                {
+                    Id = 90,
+                    Name = "Alemania",
+                    Continent = "Europe",
+                    FlagUrl = "http://localhost:3000/img/germany-flag-button-square-xs.png",
+
+                },
+                new Pais
+                {
+                    Id = 91,
+                    Name = "España",
+                    Continent = "Europe",
+                    FlagUrl = "http://localhost:3000/img/spain-flag-button-square-xs.png",
+
+                },
+                new Pais
+                {
+                    Id = 92,
+                    Name = "Portugal",
+                    Continent = "Europe",
+                    FlagUrl = "http://localhost:3000/img/portugal-flag-button-square-xs.png",
+
+                },
+                 new Pais
+                {
+                    Id = 93,
+                    Name = "Francia",
+                    Continent = "Europe",
+                    FlagUrl = "http://localhost:3000/img/france-flag-button-square-xs.png",
+
+                },
+                  new Pais
+                {
+                    Id = 94,
+                    Name = "Russia",
+                    Continent = "Europe",
+                    FlagUrl = "http://localhost:3000/img/russia-flag-button-square-xs.png",
+
+                },
+                   new Pais
+                {
+                    Id = 95,
+                    Name = "Inglaterra",
+                    Continent = "Europe",
+                    FlagUrl = "http://localhost:3000/img/united-kingdom-flag-button-square-xs.png",
+
+                }
+            };
+        }
+        private static List<Pais> ObtienePaisesDeAmerica()
+        {
+            return new List<Pais>
+            {
+                new Pais
+                {
+                    Id = 1,
+                    Name = "Argentina",
+                    Continent = "America",
+                    FlagUrl = "http://localhost:3000/img/argentina-flag-button-square-xs.png",
+
+                },
+                new Pais
+                {
+                    Id = 2,
+                    Name = "Brazil",
+                    Continent = "America",
+                    FlagUrl = "http://localhost:3000/img/brazil-flag-button-square-xs.png",
+
+                },
+                new Pais
+                {
+                    Id = 3,
+                    Name = "Costa Rica",
+                    Continent = "America",
+                    FlagUrl = "http://localhost:3000/img/costa-rica-flag-button-square-xs.png",
+
+                },
+                 new Pais
+                {
+                    Name = "Uruguay",
+                    Continent = "America",
+                    FlagUrl = "http://localhost:3000/img/uruguay-flag-3d-xs.png",
+
+                },
+                  new Pais
+                {
+                    Id = 4,
+                    Name = "Paraguay",
+                    Continent = "America",
+                    FlagUrl = "http://localhost:3000/img/paraguay-flag-button-square-xs.png",
+
+                },
+                   new Pais
+                {
+                    Id = 5,
+                    Name = "Colombia",
+                    Continent = "America",
+                    FlagUrl = "http://localhost:3000/img/colombia-flag-button-square-xs.png",
+
+                },
+                new Pais
+                {
+                    Id = 6,
+                    Name = "Peru",
+                    Continent = "America",
+                    FlagUrl = "http://localhost:3000/img/notfound.png",
+
+                },
+                new Pais
+                {
+                    Id = 7,
+                    Name = "Bolivia",
+                    Continent = "America",
+                    FlagUrl = "http://localhost:3000/img/notfound.png",
+
+                },
+                new Pais
+                {
+                    Id = 8,
+                    Name = "Venezuela",
+                    Continent = "America",
+                    FlagUrl = "http://localhost:3000/img/notfound.png",
+
+                }
+            };
         }
 
         // GET api/values/5
@@ -42,4 +180,11 @@ namespace API.Controllers
         {
         }
     }
+}
+public class Pais
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string FlagUrl { get; set; }
+    public string Continent { get; set; }
 }
